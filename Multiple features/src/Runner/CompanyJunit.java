5a9c03b8ce1648dd0e0d608632d58a0b7ce193f7
priyam <priyamghosh39@gmail.com>
2019-02-26 20:00:54 +0530

    comment added

--- Multiple features/src/Runner/CompanyJunit.java
+++ Multiple features/src/Runner/CompanyJunit.java
@@ -4,6 +4,8 @@ import org.junit.runner.RunWith;
 
 import cucumber.api.junit.Cucumber;
 
+//Bapi bari jaa
+
 @RunWith(Cucumber.class)
 @Cucumber.Options(features = "Feature", glue={"StepDefinition"})
 public class CompanyJunit 
